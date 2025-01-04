f49b460f9ebee12ab7805abf21894a73585368fd
Lahcen ELORF <lahcenelorf.24@gmail.com>
2025-01-04 17:43:32 +0100

    Finish the project

--- src/quiz/app/Login.java
+++ src/quiz/app/Login.java
@@ -7,58 +7,59 @@ import java.awt.event.ActionListener;
 
 public class Login extends JFrame implements ActionListener {
     JTextField text;
-    JButton next, back;
-    Login() {
+    JButton Next, back;
+    Login(){
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/login.png"));
-        Image i = i1.getImage().getScaledInstance(550, 500, Image.SCALE_DEFAULT);
+        Image i = i1.getImage().getScaledInstance(550,500,Image.SCALE_DEFAULT);
         ImageIcon i2 = new ImageIcon(i);
         JLabel image = new JLabel(i2);
-        image.setBounds(450, 0, 550, 500);
+        image.setBounds(450,0,550,500);
         add(image);
 
         JLabel heading = new JLabel("QUIZ TEST");
-        heading.setBounds(140, 60, 300, 45);
-        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 40));
-        heading.setForeground(new Color(22, 99, 54));
+        heading.setBounds(140,60,300,45);
+        heading.setFont(new Font("Viner Hand ITC",Font.BOLD,40));
+        heading.setForeground(new Color(22,99,54));
         add(heading);
 
         JLabel name = new JLabel("Enter Your Name");
-        name.setBounds(160, 150, 300, 20);
-        name.setFont(new Font("Mongolian Baiti", Font.BOLD, 18));
-        name.setForeground(new Color(22, 99, 54));
+        name.setBounds(160,150,300,20);
+        name.setFont(new Font("Mongolian Baiti", Font.BOLD,18));
+        name.setForeground(new Color(22,99,54));
         add(name);
 
         text = new JTextField();
-        text.setBounds(80, 200, 300, 25);
-        text.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        text.setBounds(80,200,300,25);
+        text.setFont(new Font("Times New Roman", Font.BOLD,18));
         add(text);
 
-        next = new JButton("Next");
-        next.setBounds(100, 270, 120, 25);
-        next.setBackground(new Color(22, 99, 54));
-        next.setForeground(Color.WHITE);
-        next.addActionListener(this);
-        add(next);
-
+        Next = new JButton("Next");
+        Next.setBounds(100,270,120,25);
+        Next.setBackground(new Color(22,99,54));
+        Next.setForeground(Color.WHITE);
+        Next.addActionListener(this);
+        add(Next);
 
         back = new JButton("Back");
-        back.setBounds(250, 270,120, 25);
-        back.setBackground(new Color(22, 99, 54));
+        back.setBounds(250,270,120,25);
+        back.setBackground(new Color(22,99,54));
         back.setForeground(Color.WHITE);
         back.addActionListener(this);
         add(back);
 
-        setSize(1000, 500);
-        setLocation(200, 150);
+
+        setSize(1000,500);
+        setLocation(200,150);
+        setUndecorated(true);
         setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == next) {
+        if (e.getSource() == Next){
             String name = text.getText();
             setVisible(false);
             new Rules(name);
